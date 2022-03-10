aec726d84174502843cd30b8835275cc36e0a237
Quedo999 <sri00119@naver.com>
2022-03-11 03:05:13 +0900

    # 2022/03/11

---
+++ 프로그래머스 코딩테스트 고득점 킷/정렬/02. 가장 큰 수.py
@@ -0,0 +1,20 @@
+def solution(numbers):
+    answer = []
+    numbers = [str(i) for i in numbers]
+
+    for i in numbers 
+
+    return max(answer)
+
+
+
+
+# # 테스트 케이스 1
+# numbers = [6, 10, 2]
+# # return : 6210
+
+# 테스트 케이스 2
+numbers = [3, 30, 34, 5, 9]
+# return : 9534330
+
+print(solution(numbers))
